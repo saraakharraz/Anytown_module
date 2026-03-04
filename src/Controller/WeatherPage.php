a65456db1fe15cb53e1a222a05465f85dc49037b
sara akharraz <s.akharraz@void.fr>
2026-03-04 14:43:54 +0000

    services and dependency injection

--- src/Controller/WeatherPage.php
+++ src/Controller/WeatherPage.php
@@ -1,35 +1,77 @@
-<?php
+<?php 
 
 declare(strict_types=1);
 
 namespace Drupal\anytown\Controller;
 
+use Drupal\anytown\ForecastClientInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Controller for anytown.weather_page route.
  */
 class WeatherPage extends ControllerBase {
 
-/**
- * Builds the response.
- */
-  public function build(string $style):array{
+  /**
+   * Forecast API client.
+   *
+   * @var \Drupal\anytown\ForecastClientInterface
+   */
+  private $forecastClient;
 
-    $style = (in_array($style,['short','extended']))? $style:'short';
+  /**
+   * WeatherPage controller constructor.
+   *
+   * @param \Drupal\anytown\ForecastClientInterface $forecast_client
+   *   Forecast API client service.
+   */
+  public function __construct(ForecastClientInterface $forecast_client) {
+    $this->forecastClient = $forecast_client;
+  }
 
-    $build['content']=[
-        '#type' => 'markup',
-        '#markup' =>'<p>The weather forecast for this week is sunny </p>'
-    ];
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('anytown.forecast_client')
+    );
+  }
 
-    if($style === 'extended'){
-        $build['content_extended'] = [
-            '#type'=> 'markup',
-            '#markup' =>'<p><strong>Extended forcast :</strong>Looking ahead to next week we expect some snow</p>',
-        ];
+  /**
+   * Builds the response.
+   */
+  public function build(string $style): array {
+    // Style should be one of 'short', or 'extended'. And default to 'short'.
+    $style = (in_array($style, ['short', 'extended'])) ? $style : 'short';
+
+    $url = 'https://raw.githubusercontent.com/DrupalizeMe/module-developer-guide-demo-site/main/backups/weather_forecast.json';
+    $forecast_data = $this->forecastClient->getForecastData($url);
+    if ($forecast_data) {
+      $forecast = '<ul>';
+      foreach ($forecast_data as $item) {
+        [
+          'weekday' => $weekday,
+          'description' => $description,
+          'high' => $high,
+          'low' => $low,
+        ] = $item;
+        $forecast .= "<li>$weekday will be <em>$description</em> with a high of $high and a low of $low.</li>";
+      }
+      $forecast .= '</ul>';
+    }
+    else {
+      $forecast = '<p>Could not get the weather forecast. Dress for anything.</p>';
     }
 
-    return $build;
+    $output = "<p>Check out this weekend's weather forecast and come prepared. The market is mostly outside, and takes place rain or shine.</p>";
+    $output .= $forecast;
+    $output .= '<h3>Weather related closures</h3></h3><ul><li>Ice rink closed until winter - please stay off while we prepare it.</li><li>Parking behind Apple Lane is still closed from all the rain last week.</li></ul>';
+
+    return [
+      '#markup' => $output,
+    ];
   }
+
 }
